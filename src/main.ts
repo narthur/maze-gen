5c97368824812e0e7ac64870d1d931c0db13ab99
Nathan Arthur <nathan@nathanarthur.com>
2024-11-16 17:14:45 -0500

    referral links

--- src/main.ts
+++ src/main.ts
@@ -25,3 +25,15 @@ function step() {
 }
 
 step();
+
+const referralLinks = [
+  "https://codebuff.com/referrals/ref-6d348d54-80f1-4155-903b-2cc6c57dd12f",
+  "https://codebuff.com/referrals/ref-60623de5-01be-4471-9ebd-2844792dd65d",
+];
+const randomLink =
+  referralLinks[Math.floor(Math.random() * referralLinks.length)];
+const referralLink = document.querySelector<HTMLAnchorElement>("#codebuff")!;
+
+referralLink.href = randomLink;
+referralLink.title =
+  "Referral link for extra 500 credits / month for both of us 💚";
